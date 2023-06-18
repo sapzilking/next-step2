d6707a580c59057ee5504393399bc8095cebfb76
sapzilking <sapzilking@gmail.com>
2023-06-18 18:25:59 +0900

    6.5 MVC 프레임워크 구현 1단계 실습

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -1,23 +1,16 @@
 package next.controller;
 
 import core.db.DataBase;
+import core.mvc.Controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet("")
-public class HomeController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+public class HomeController implements Controller {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         req.setAttribute("users", DataBase.findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
-        rd.forward(req, resp);
+        return "home.jsp";
     }
 }
--- src/main/java/next/controller/LogoutController.java
+++ src/main/java/next/controller/LogoutController.java
@@ -1,20 +1,17 @@
 package next.controller;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import core.mvc.Controller;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
-@WebServlet("/users/logout")
-public class LogoutController extends HttpServlet {
+public class LogoutController implements Controller {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         HttpSession session = req.getSession();
         session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
-        resp.sendRedirect("/");
+        return "redirect:/";
     }
 }
