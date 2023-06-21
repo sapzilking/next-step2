8575dd775d5d249433898271ddd3de1bd22d6db1
sapzilking <sapzilking@gmail.com>
2023-06-21 21:49:43 +0900

    AJAX 답변하기 기능 구현

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,3 +1,30 @@
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+  e.preventDefault(); // submit이 자동으로 동작하는 것을 막는다.
+  //form 데이터들을 자동으로 묶어준다.
+  var queryString = $("form[name=answer]").serialize();
+
+  $.ajax({
+    type : 'post',
+    url : '/api/qna/addAnswer',
+    data : queryString,
+    dataType : 'json',
+    error : onError,
+    success : onSuccess,
+  });
+}
+
+function onSuccess(json, status) {
+  var answerTemplate = $("#answerTemplate").html();
+  var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId);
+  $(".qna-comment-slipp-articles").prepend(template);
+}
+
+function onError(xhr, status) {
+  alert("error");
+}
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
